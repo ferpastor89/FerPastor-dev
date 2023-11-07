1f1ca4c17ef27c5d9659166cd46990434a7e7431
Fernando Pastor <fernandoandres.pastor@gmail.com>
2023-11-07 08:32:49 -0300

    Cambios en menú desplegable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,23 +10,26 @@ const Navbar = () => {
   return (
     <div className="fixed top-4 right-4 z-50">
       <div className="lg:hidden">
-        <button className="menu-hamburguesa focus:outline-none text-white" onClick={toggleMenu}>
+        <button className="menu-hamburguesa focus:outline-none text-white z-10" onClick={toggleMenu}>
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            {menuAbierto ? (
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-            ) : (
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            )}
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 6h22M7 12h17M10 18h15"></path>
           </svg>
         </button>
+        {menuAbierto && (
+          <button className="menu-cerrar absolute top-4 right-4 focus:outline-none text-white z-20" onClick={toggleMenu}>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+          </button>
+        )}
       </div>
       {menuAbierto && (
-        <div className="fixed top-20 left-1/2 transform -translate-x-1/2 w-80 z-50">
-          <div className="p-6 text-black text-center bg-yellow-300">
-            <a href="#home" className="text-xl block py-2 hover:text-gray-300 ">Home</a>
-            <a href="#Projects" className="text-xl block py-2 hover:text-gray-300 ">Projects</a>
-            <a href="#skills" className="text-xl block py-2 hover:text-gray-300 ">Skills</a>
-            <a href="#about" className="text-xl block py-2 hover:text-gray-300 ">about</a>
+        <div className="fixed top-0 left-0 w-full h-full bg-yellow-300 bg-opacity-60 backdrop-blur-lg flex justify-center items-center z-15">
+          <div className="w-80 p-6  text-white text-center text-3xl bold uppercase pt-0">
+            <a href="#home" className=" block py-6 hover:text-gray-500 transition duration-300 transform hover:scale-105">Home</a>
+            <a href="#Projects" className=" block py-6 hover:text-gray-500 transition duration-300 transform hover:scale-105">Projects</a>
+            <a href="#skills" className=" block py-6 hover:text-gray-500 transition duration-300 transform hover:scale-105">Skills</a>
+            <a href="#about" className=" block py-6 hover:text-gray-500 transition duration-300 transform hover:scale-105">About</a>
           </div>
         </div>
       )}
